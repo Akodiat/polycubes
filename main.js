ef5faf2b2ed665faacb4305b8bca7964bdfb75e1
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2019-02-28 18:05:36 +0000

    Changed move queue, added move processing

--- main.js
+++ main.js
@@ -11,6 +11,9 @@ var cubeGeo, cubeMaterial;
 
 var objects = [];
 var moves = {};
+var moveKeys = [];
+
+var rules = [[0,0,0,0,0,2]]
 
 var ruleOrder = [
     new THREE.Vector3( 0, 0,-1),
@@ -23,6 +26,31 @@ var ruleOrder = [
 
 init();
 render();
+
+// From answer: https://stackoverflow.com/a/14853974
+function arrayEquals(a,b) {
+    // if the other array is a falsy value, return
+    if (!b)
+        return false;
+
+    // compare lengths - can save a lot of time 
+    if (a.length != b.length)
+        return false;
+
+    for (var i = 0, l=a.length; i < l; i++) {
+        // Check if we have nested arrays
+        if (a[i] instanceof Array && b[i] instanceof Array) {
+            // recurse into the nested arrays
+            if (!a[i].equals(b[i]))
+                return false;       
+        }           
+        else if (a[i] != b[i]) { 
+            // Warning - two different object instances will never be equal: {x:20} != {x:20}
+            return false;   
+        }           
+    }       
+    return true;
+}
 
 function init() {
     camera = new THREE.PerspectiveCamera(45, window.innerWidth / window
@@ -144,11 +172,25 @@ function onDocumentMouseDown(event) {
         // create cube
         } else {
             pos = intersect.point.clone().add(intersect.face.normal).floor();
-            addCube(pos, [0,1,2,0,0,0]);
+            addCube(pos, [0,1,2,0,-1,0]);
         }
 
         render();
     }
+}
+
+function processMoves() {
+    // Select random move destination
+    var moveKey = Math.floor(Math.random()*moveKeys.length);
+    var move = moves[moveKeys[moveKey]];
+
+    rules.forEach(function(rule){
+        if(arrayEquals(rule,move)){
+            console.log("Yay");
+            delete moves[moveKeys[moveKey]];
+            moveKeys.splice(moveKey, 1);
+        }
+    }); 
 }
 
 function addCube(position, rule) {
@@ -160,20 +202,31 @@ function addCube(position, rule) {
     
     for(i=0; i<rule.length; i++) {
         if(rule[i]) {
-           var move = {
-               position: position.clone().add(ruleOrder[i]),
-               direction: ruleOrder[i].clone().negate(),
-               rule: rule[i]
-           }
-           var key = `(${move.position.x},${move.position.y},${move.position.z})`;
+     //    var move = {
+     //        position: position.clone().add(ruleOrder[i]),
+     //        direction: ruleOrder[i].clone().negate(),
+     //        rule: rule[i]
+     //    }
+           var direction = ruleOrder[i].clone().negate();
+           var pos = position.clone().add(ruleOrder[i])
+           var key = `(${pos.x},${pos.y},${pos.z})`;
            if(!(key in moves)) {
-               moves[key] = [];
-           } else {
-               console.log("Non deterministic move:", move);
+               moves[key] = [0,0,0,0,0,0];
+               moveKeys.push(key);
            }
-           moves[key].push(move); 
+           var r = position.clone().sub(pos);
+           var rulePos = ruleOrder.findIndex(function(element){return r.equals(element)});
+
+           //Make sure we haven't written anything here before:
+           console.assert(!(moves[key][rulePos]));
+
+           moves[key][rulePos] = rule[i] * -1;
         }
     }
+}
+
+function vecToStr(v){
+    return `(${v.x},${v.y},${v.z})`;
 }
 
 function onDocumentKeyDown(event) {
@@ -194,4 +247,6 @@ function onDocumentKeyUp(event) {
 
 function render() {
     renderer.render(scene, camera);
+    processMoves();
 }
+
