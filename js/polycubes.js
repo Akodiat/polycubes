522379b89ca2ede74de941ee40b8023e4e47a9a0
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2019-03-13 14:56:30 +0000

    Remove unused listeners

--- js/polycubes.js
+++ js/polycubes.js
@@ -196,10 +196,6 @@ function init() {
 
     document.addEventListener('mousemove', onDocumentMouseMove, false);
     document.addEventListener('mousedown', onDocumentMouseDown, false);
-    document.addEventListener('keydown', onDocumentKeyDown, false);
-    document.addEventListener('keyup', onDocumentKeyUp, false);
-
-    //
 
     window.addEventListener('resize', onWindowResize, false);
 }
