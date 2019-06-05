62adfa7bc75abb2208c16de2f503a611993330e5
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2019-06-05 15:53:33 +0100

    [cpp] Add loop in run script

--- cpp/run.sh
+++ cpp/run.sh
@@ -1,6 +1,17 @@
 mkdir -p out
 cd out
-rule=$(../bin/randRule)
-../bin/polycubes $rule &> /dev/null
-echo $?
+
+if [ -z "$1" ]; then
+    echo "Running once"
+    N_TIMES=1
+else
+    N_TIMES=$1
+fi
+
+echo "Running $N_TIMES times"
+for i in $(seq $N_TIMES); do
+    RULE=$(../bin/randRule)
+    ../bin/polycubes $RULE &> /dev/null &
+done
+
 cd ..
