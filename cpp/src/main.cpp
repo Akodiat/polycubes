b1d68647762d140416a5cea2d767f8e8484166db
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2019-11-13 14:51:46 +0000

    [cpp] Rewrite i/o behaviour
    Will now store everything in one file per process.
    Also allows for input options.

--- cpp/src/main.cpp
+++ cpp/src/main.cpp
@@ -2,8 +2,13 @@
 #include <fstream>
 #include <vector>
 #include "polycubeSystem.hpp"
-#include <cmath>
 #include <bitset>
+#include <getopt.h>
+#include <random>
+#include <chrono>
+#include <bitset>
+#include <math.h>
+#include <unistd.h>
 
 // 0 sign
 // 0 value 16
@@ -18,53 +23,107 @@
 // 32 possible values (colours)
 // Need 2 hexadecimal digits.
 
+std::string randRule(int maxColor, int maxCubes) {
+    const int colBits = 5;
+    const int rotBits = 2;
+    //int maxColor = pow(2, colBits); // 32;
+    int nRotations = pow(2, rotBits); // 4;
 
-int main(int argc, char** argv) {
-    //std::cout<<"Welcome to polycubes!"<<std::endl;
-
-    static int nTries = 5;
-    if (argc > 1) {
-        int nCubes = 0;
-        std::ofstream fs;
-        std::string s = "";
-        while(nTries--) {
-            PolycubeSystem p =
-                argc > 2 ?
-                PolycubeSystem(argv[1], std::stoi(argv[2])) :
-                PolycubeSystem(argv[1]);
-            p.addCube(Eigen::Vector3f(0,0,0), 0);
-            nCubes = p.processMoves();
-            if (nCubes <= 0) {
-                fs.open(
-                    std::string(argv[1]) +
-                    ".oub" +
-                    std::to_string(p.getNMaxCubes())
-                );
-                fs.close();
-                return -1;
-            }
-            std::string s_new = p.toString();
-            if (s == "") {
-                s = s_new;
-            }
-            else if (s != s_new) {
-                fs.open(
-                    std::string(argv[1]) +
-                    ".nondet"
-                );
-                fs.close();
-                return -1;
-            }
+    unsigned seed = std::chrono::system_clock::now().time_since_epoch().count();
+    int hex = 16;
+    std::mt19937 r(seed);
+    std::uniform_int_distribution<size_t> signDist(0, 1);
+    std::uniform_int_distribution<size_t> colorDist(0, maxColor-1);
+    std::uniform_int_distribution<size_t> rotDist(0, nRotations-1);
+    std::uniform_int_distribution<size_t> nRuleDist(1, maxCubes);
+    int nCubes = nRuleDist(r);
+    char ruleBuf[nCubes*12];
+    strcpy(ruleBuf, "");
+    while (nCubes --) {
+        for(int i=0; i<6; i++) {
+            int sign  = signDist(r);
+            int color = colorDist(r);
+            int rot   = rotDist(r);
+            std::string signStr = std::bitset<1>(sign).to_string();
+            std::string colorStr = std::bitset<colBits>(color).to_string();
+            std::string rotStr = std::bitset<rotBits>(rot).to_string();
+            std::string totStr = signStr + colorStr + rotStr;
+            int rule = std::stoi(totStr, 0, 2);
+            sprintf(ruleBuf+strlen(ruleBuf),"%02x", rule);
         }
-        // If we had the same result every try:
-        fs.open(std::string(argv[1])+"."+std::to_string(nCubes)+"-mer");
-        fs << s;
-        fs.close();
-        return nCubes;
     }
-    else {
-        std::cout<<"Sorry, you need to supply a rule"<<std::endl;
+    return std::string(ruleBuf);
+}
+PolycubeResult* runTries(std::string rule, int nTries)
+{
+    int nCubes = 0;
+    std::string s = "";
+    while (nTries--) {
+        PolycubeSystem p = PolycubeSystem(rule);
+        p.addCube(Eigen::Vector3f(0, 0, 0), 0);
+        nCubes = p.processMoves();
+
+        if (nCubes <= 0) {
+            return new PolycubeResult(std::string(rule), "oub" + std::to_string(p.getNMaxCubes()));
+        }
+        std::string s_new = p.toString();
+        if (s == "") {
+            s = s_new;
+        }
+        else if (s != s_new) {
+            return new PolycubeResult(std::string(rule), "nondet");
+        }
+    }
+    // If we had the same result every try:
+    return new InterestingPolycubeResult(std::string(rule), nCubes, s);
+}
+
+static struct option long_options[] = {
+    {"rule", optional_argument, NULL, 'r'},
+    {"maxColors", optional_argument, NULL, 'c'},
+    {"maxRulesize", optional_argument, NULL, 's'},
+    {"nTimes", optional_argument, NULL, 'n'},
+    {"nTries", optional_argument, NULL, 't'},
+    {NULL, 0, NULL, 0}
+};
+
+int main(int argc, char **argv) {
+    std::string rule = "";
+    int maxColors = 8;   // Max number of colors to use in random rule
+    int maxRulesize = 8; // Max size of random rule
+    int nTimes = 1;      // Number of random rules to generate
+    int nTries = 5;      // Number of tries to determine if a rule is deterministic
+    // loop over all of the options
+    int ch;
+    while ((ch = getopt_long(argc, argv, "r:c:s:n:t:", long_options, NULL)) != -1) {
+        switch (ch) {
+        case 'r': rule = std::string(optarg); break;
+        case 'c': maxColors = std::stoi(optarg); break;
+        case 's': maxRulesize = std::stoi(optarg); break;
+        case 'n': nTimes = (int)std::stod(optarg); break;
+        case 't': nTries = std::stoi(optarg); break;
+        }
     }
 
-    return -2;
+    if (rule.length() > 0) {
+        // Run with the rule provided, ignoring other parameters
+        PolycubeResult* result = runTries(rule, nTries);
+        if(result->isInteresting()) {
+            InterestingPolycubeResult* interestingRes = 
+                dynamic_cast<InterestingPolycubeResult*>(result);
+            std::cout << interestingRes->getCoords();
+        } else {
+            std::cout << result->toString() << std::endl;
+        }
+    } else {
+        std::ofstream fs;
+        int tmp = getpid(); char pid[100]; sprintf(pid, "%d", tmp);
+        fs.open("out_"+std::string(pid));
+        while (nTimes--) {
+            rule = randRule(maxColors, maxRulesize);
+            PolycubeResult* result = runTries(rule, nTries);
+            fs << (result->toString()) << std::endl;
+        }
+        fs.close();
+    }
 }
--- cpp/src/polycubeSystem.hpp
+++ cpp/src/polycubeSystem.hpp
@@ -9,45 +9,91 @@
 #include "../lib/Eigen/Dense"
 #include "../lib/Eigen/Geometry"
 
+class PolycubeResult
+{
+private:
+    std::string rule;
+    std::string suffix;
+public:
+    PolycubeResult(std::string rule, std::string suffix) {
+        this->rule = rule;
+        this->suffix = suffix;
+    }
+    std::string toString() {
+        return this->rule + '.' + this->suffix;
+    };
+
+    virtual bool isInteresting() {
+        return false;
+    }
+};
+
+class InterestingPolycubeResult: public PolycubeResult
+{
+private:
+    std::string coords;
+public:
+    InterestingPolycubeResult(std::string rule, int polycubeSize, std::string coords): PolycubeResult(rule, std::to_string(polycubeSize) + "-mer")
+    {
+        this->coords = coords;
+    }
+    std::string getCoords() {
+        return this->coords;
+    }
+    bool isInteresting() {
+        return true;
+    }
+};
+
 const size_t ruleSize = 6;
 
-class Face {
+class Face
+{
 private:
     Eigen::Vector3f orientation;
-    int* color;
+    int *color;
+
 public:
-    Face(int color, Eigen::Vector3f orientation) {
+    Face(int color, Eigen::Vector3f orientation)
+    {
         this->color = new int(color);
         this->orientation = orientation;
     }
-    Face(Eigen::Vector3f orientation) {
+    Face(Eigen::Vector3f orientation)
+    {
         this->color = nullptr;
         this->orientation = orientation;
     }
-    Eigen::Vector3f getOrientation() {
+    Eigen::Vector3f getOrientation()
+    {
         return this->orientation;
     }
-    bool hasColor() {
+    bool hasColor()
+    {
         return this->color != nullptr;
     }
-    int getColor(){
+    int getColor()
+    {
         return *(this->color);
     }
-    int setColor(int color) {
+    int setColor(int color)
+    {
         this->color = &color;
     }
 };
 
-typedef std::array<Face*,ruleSize> Rule;
+typedef std::array<Face *, ruleSize> Rule;
 
-struct POTENTIAL_MOVE {
+struct POTENTIAL_MOVE
+{
     std::string key;
     size_t dirIdx;
     int val;
     Eigen::Vector3f orientation;
 };
 
-class Move {
+class Move
+{
 private:
     Eigen::Vector3f movePos;
     Rule rule;
@@ -55,19 +101,22 @@ private:
 public:
     Move(Eigen::Vector3f movePos);
 
-    Rule getRule() {
+    Rule getRule()
+    {
         return this->rule;
     }
-    Eigen::Vector3f getMovePos() {
+    Eigen::Vector3f getMovePos()
+    {
         return this->movePos;
     }
-    void setRuleAt(int i, Face* f) {
+    void setRuleAt(int i, Face *f)
+    {
         this->rule[i] = f;
     }
 };
 
-
-class PolycubeSystem {
+class PolycubeSystem
+{
 public:
     PolycubeSystem(std::vector<Rule> rules);
     PolycubeSystem(std::string rules);
@@ -90,22 +139,21 @@ private:
 
     std::unordered_map<std::string, Move> moves;
     std::vector<std::string> moveKeys;
-    std::map<std::string,bool> cubeMap;
+    std::map<std::string, bool> cubeMap;
     int nMaxCubes;
 
     Eigen::Vector3f ruleOrder[ruleSize];
-    std::map<std::string,size_t> ruleToOrderIdx;
+    std::map<std::string, size_t> ruleToOrderIdx;
     std::vector<Rule> rules;
 
     void init(std::vector<Rule> rules, int nMaxCubes);
 
-    Rule* ruleFits(Rule a, Rule b);
+    Rule *ruleFits(Rule a, Rule b);
 
     float getSignedAngle(
         Eigen::Vector3f v1,
         Eigen::Vector3f v2,
-        Eigen::Vector3f axis
-    );
+        Eigen::Vector3f axis);
 
     Rule rotateRule(Rule rule, Eigen::Quaternion<float> q);
     Rule rotateRuleFromTo(Rule rule, Eigen::Vector3f vFrom, Eigen::Vector3f vTo);
