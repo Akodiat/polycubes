4e40844e2f9778a7fb97ec160e0c827ecbac3899
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2020-10-27 19:03:12 +0000

    [py] Request more memory calculating phenos

--- py/getPhenos_slurm.sh
+++ py/getPhenos_slurm.sh
@@ -1,4 +1,4 @@
 for f in "$1"/nmers/*-mers_*.p; do
     n=$(basename "$f")
-    addqueue -o "$n-%j.out" -c "Find phenos $n" -n 1 -m 1 /usr/bin/python3 $(dirname "$0")/getPhenosForNMer.py "$f"
+    addqueue -o "$n-%j.out" -c "Find phenos $n" -n 1 -m 6 /usr/bin/python3 $(dirname "$0")/getPhenosForNMer.py "$f"
 done
