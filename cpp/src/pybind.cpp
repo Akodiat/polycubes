c673d3a470229083ad1df21628f460aec0856d44
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2020-11-19 11:46:46 +0000

    [js} Add coordinate export to pybind

--- cpp/src/pybind.cpp
+++ cpp/src/pybind.cpp
@@ -32,6 +32,28 @@ bool isBoundedAndDeterministic(std::string rule, int nTries, int seedRuleIdx) {
     return result != "oub" && result != "nondet";
 }
 
+std::string getCoordStr(std::string rule, int seedRuleIdx)
+{
+    PolycubeSystem* p = new PolycubeSystem(rule);
+    p->seed(seedRuleIdx);
+    p->processMoves();
+    std::string s = p->toString();
+    delete p;
+    return s;
+}
+
+/*
+Eigen::Matrix3Xf getCoords(std::string rule, int seedRuleIdx)
+{
+    PolycubeSystem* p = new PolycubeSystem(rule);
+    p->seed(seedRuleIdx);
+    p->processMoves();
+    Eigen::Matrix3Xf m = p->getCoordMatrix();
+    delete p;
+    return m;
+}
+*/
+
 PYBIND11_MODULE(polycubes, m) {
     m.doc() = "Polycube python binding";
     m.def("checkEquality", &checkEquality, "Compare if the two rules form the same polycube");
@@ -39,4 +61,5 @@ PYBIND11_MODULE(polycubes, m) {
         "Check if the rule is bounded and form the same polycube every time (set nTries to 0 to only check if bounded)",
         "rule"_a, "nTries"_a = 15, "seedRuleIdx"_a = -1
     );
+    m.def("getCoordStr", &getCoordStr, "Get coordinate string of assembled polycube", "rule"_a, "seedRuleIdx"_a = -1);
 }
