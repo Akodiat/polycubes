51354e178e52fd3f0f6cee434a9de873600ea27d
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2020-11-30 21:08:41 +0000

    [cpp] Update analysis bash

--- cpp/analyse.sh
+++ cpp/analyse.sh
@@ -1,1 +1,14 @@
-addqueue -s -q "bigmem" -c "Analyse 3d 2 hours" -o "sortNMers_%j.out" -n 1 -m 128 /usr/bin/python3 ~/repo/polycubes/py/sortNMers.py out_3d
+slurm=false
+
+for f in "$1"/*-mers; do
+    echo "Starting merge of $f"
+    if [ "$slurm" = true ] ; then
+        addqueue -s -q "bigmem" -c "Analyse $f 2 hours" -o "sortNMers_%j.out" -n 1 -m 128 /usr/bin/python3 $(dirname "$0")/../py/mergeOutput.py "$f"
+    else
+        python $(dirname "$0")/../py/mergeOutput.py "$f" &
+    fi
+done
+
+wait
+
+echo "Done"
