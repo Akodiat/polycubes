9f5bde4f0661e7ebe0276b1cfb61f41b0daf061c
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2020-12-03 11:11:14 +0000

    [cpp] Change slurm command to allocate less memory

--- cpp/analyse.sh
+++ cpp/analyse.sh
@@ -1,9 +1,9 @@
-slurm=false
+slurm=true
 
 for f in "$1"/*-mers; do
     echo "Starting merge of $f"
     if [ "$slurm" = true ] ; then
-        addqueue -s -q "bigmem" -c "Analyse $f 2 hours" -o "sortNMers_%j.out" -n 1 -m 128 /usr/bin/python3 $(dirname "$0")/../py/mergeOutput.py "$f"
+        addqueue -s -c "Analyse $f 2 hours" -o "sort-%j.out" -n 1 -m 8 /usr/bin/python3 $(dirname "$0")/../py/mergeOutput.py "$f"
     else
         python $(dirname "$0")/../py/mergeOutput.py "$f" &
     fi
