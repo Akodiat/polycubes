8ec72250a000061fb214a6b450ad60a31865d5cd
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2020-12-31 16:56:58 +0000

    [cpp] Fix merge script slurm typo

--- cpp/merge.sh
+++ cpp/merge.sh
@@ -14,7 +14,7 @@ echo "Done"
 for f in "$1"/*-mers; do
     echo "Starting merge of $f"
     if [ "$slurm" = true ]; then
-        addqueue -s -c "Analyse $f 2 hours" -o "sort-%j.out" -n 1 -m 8 /usr/bin/python3 $(dirname "$0")/../py/mergeOutput.py "$f"
+        addqueue -s -c "Merge $f 2 hours" -o "merge-%j.out" -n 1 -m 8 /usr/bin/python3 $(dirname "$0")/../py/mergeOutput.py "$f" $$
     else
         python $(dirname "$0")/../py/mergeOutput.py "$f" $$ &
     fi
