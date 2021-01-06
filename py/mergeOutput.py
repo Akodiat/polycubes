dc2b6eed4ee84d9355100039084ff6cbd5af9b61
Joakim Bohlin <joakim.bohlin@physics.ox.ac.uk>
2021-01-06 11:53:11 +0000

    [py] Improve merge script

--- py/mergeOutput.py
+++ py/mergeOutput.py
@@ -5,10 +5,17 @@ import re
 import pickle
 import sys
 
+# Get just the first rule from pheno file
+def getRuleFromPheno(path):
+    with open(path) as f:
+        return f.readline().strip()
+
 def mergeOutput(nmerdir, pid):
-    for root, _, conffiles in os.walk(os.path.dirname(os.path.abspath(nmerdir))):
-        confs = [utils.readConf(os.path.join(root, c)) for c in conffiles if ".conf" in c]
+    for r, _, cs in os.walk(os.path.dirname(os.path.abspath(nmerdir))):
+        assemblyModes = set(utils.readConf(os.path.join(r, c))['assemblyMode'] for c in cs if ".conf" in c)
         break
+    assert len(assemblyModes) == 1, "Incompatable assembly modes"
+    assemblyMode = assemblyModes.pop()
 
     phenos = {}
     # For each phenotype file
@@ -20,13 +27,16 @@ def mergeOutput(nmerdir, pid):
                     'sizeId': r.group(1),
                     'index': r.group(2),
                     'pID': r.group(3),
-                    'phenos': utils.getRulesFromPheno(os.path.join(root, phenoFile))
+                    'rule': getRuleFromPheno(os.path.join(root, phenoFile)),
+                    'path': os.path.join(root, 'merged', phenoFile)
                 }
                 if not pheno['sizeId'] in phenos:
                     phenos[pheno['sizeId']] = []
                 phenos[pheno['sizeId']].append(pheno)
             os.renames(os.path.join(root, phenoFile), os.path.join(root, 'merged', phenoFile))
         break
+
+
 
     #phenosn = []
     # For all phenotypes with the same dimensions
@@ -36,21 +46,23 @@ def mergeOutput(nmerdir, pid):
             foundGroup = False
             for group in groups:
                 if (p['pID'] not in group['pIDs'] and 
-                    polycubes.checkEquality(p['phenos'][0], group['phenos'][0], confs[0]['assemblyMode'])
+                    polycubes.checkEquality(p['rule'], group['rule'], assemblyMode)
                 ):
                     foundGroup = True
-                    group['phenos'].extend(p['phenos'])
+                    group['paths'].append(p['path'])
                     group['pIDs'].append(p['pID'])
                     break
             if not foundGroup:
                 groups.append({
-                    'phenos': p['phenos'],
-                    'pIDs': [p['pID']]
+                    'paths': [p['path']],
+                    'pIDs': [p['pID']],
+                    'rule': p['rule']
                 })
         for i, group in enumerate(groups):
-            with open(os.path.join(nmerdir, "pheno_{}_{}_{}".format(sizeId, i, pid)), "w") as f:
-                for rule in group['phenos']:
-                    f.write(rule+'\n')
+            with open(os.path.join(nmerdir, "pheno_{}_{}_{}".format(sizeId, i, pid)), "wb") as f:
+                for path in group['paths']:
+                    with open(path, "rb") as toMerge:
+                        f.write(toMerge.read())
 
 if __name__ == "__main__":
     if len(sys.argv) > 2:
